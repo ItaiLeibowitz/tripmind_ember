be94936736482ae0350aae4a5c1482b4a1cf27eb
itai leibowitz <itai.leibowitz@gmail.com>
2016-04-01 02:03:52 -0400

    can select subsections

--- app/components/sub-section.js
+++ app/components/sub-section.js
@@ -2,5 +2,28 @@ import Ember from 'ember';
 import sectionMixin from 'tripmind/mixins/section-mixin';
 
 export default Ember.Component.extend(sectionMixin, {
-	classNames:['sub-section']
+	classNames:['sub-section'],
+	actionService: Ember.inject.service(),
+	classNameBindings: ['inSelectMode', 'isSelected'],
+	inSelectMode: Ember.computed.alias('actionService.hasSelected'),
+
+	isSelected: function(){
+		var selectedIds = this.get('actionService.selectedIds');
+		return this.get('model.items').every(function(item){
+			return selectedIds.indexOf(item.get('id')) > -1;
+		})
+	}.property('model.items.[]','actionService.selectedIds.[]'),
+
+	actions: {
+		toggleSelected: function(){
+			var itemIds = this.get('model.items').map(function(item){
+				return item.get('id');
+			});
+			if (this.get('isSelected')) {
+				this.get('actionService.selectedIds').removeObjects(itemIds);
+			} else {
+				this.get('actionService.selectedIds').addObjects(itemIds);
+			}
+		}
+	}
 });
