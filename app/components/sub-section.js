8506842ee87fbddbe76353c62764998d472cd6bb
itai leibowitz <itai.leibowitz@gmail.com>
2016-04-01 12:01:59 -0400

    added selection to major sections

--- app/components/sub-section.js
+++ app/components/sub-section.js
@@ -3,27 +3,5 @@ import sectionMixin from 'tripmind/mixins/section-mixin';
 
 export default Ember.Component.extend(sectionMixin, {
 	classNames:['sub-section'],
-	actionService: Ember.inject.service(),
-	classNameBindings: ['inSelectMode', 'isSelected'],
-	inSelectMode: Ember.computed.alias('actionService.hasSelected'),
 
-	isSelected: function(){
-		var selectedIds = this.get('actionService.selectedIds');
-		return this.get('model.items').every(function(item){
-			return selectedIds.indexOf(item.get('id')) > -1;
-		})
-	}.property('model.items.[]','actionService.selectedIds.[]'),
-
-	actions: {
-		toggleSelected: function(){
-			var itemIds = this.get('model.items').map(function(item){
-				return item.get('id');
-			});
-			if (this.get('isSelected')) {
-				this.get('actionService.selectedIds').removeObjects(itemIds);
-			} else {
-				this.get('actionService.selectedIds').addObjects(itemIds);
-			}
-		}
-	}
 });
--- app/mixins/section-mixin.js
+++ app/mixins/section-mixin.js
@@ -2,14 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 	classNames: ['section-holder'],
-	classNameBindings: ['isMinimized'],
+	classNameBindings: ['isMinimized','inSelectMode', 'isSelected'],
 	isMinimized: false,
+	actionService: Ember.inject.service(),
+	inSelectMode: Ember.computed.alias('actionService.hasSelected'),
 
+	isSelected: function(){
+		var selectedIds = this.get('actionService.selectedIds');
+		return this.get('model.items').every(function(item){
+			return selectedIds.indexOf(item.get('id')) > -1;
+		})
+	}.property('model.items.[]','actionService.selectedIds.[]'),
 
 
 	actions: {
 		toggleMinimized: function () {
 			this.toggleProperty('isMinimized');
+		},
+		toggleSelected: function(){
+			var itemIds = this.get('model.items').map(function(item){
+				return item.get('id');
+			});
+			if (this.get('isSelected')) {
+				this.get('actionService.selectedIds').removeObjects(itemIds);
+			} else {
+				this.get('actionService.selectedIds').addObjects(itemIds);
+			}
 		}
 	}
 });
