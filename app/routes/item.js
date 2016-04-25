fc76a285a912c9798715bca43f7d43f34e013af1
itai leibowitz <itai.leibowitz@gmail.com>
2016-04-25 18:08:00 -0400

    removed old links loading from item route

--- app/routes/item.js
+++ app/routes/item.js
@@ -8,22 +8,13 @@ export default Ember.Route.extend({
 			store = this.get('store');
 		return store.findRecord('item', itemId)
 			.then(function (itemRecord) {
-			// load all relevant links
-				return store.findAll('potentialLink').then(function(allLinks){
-					var relevantLinks = allLinks.filter(function (l) {
-						return l.get('itemId') == itemId;
-					});
-					return {item: itemRecord, links: relevantLinks};
-				});
-			})
-			.then(function (result) {
 			   	return store.findAll('item').then(function(items){
 					var descendants = items.filter(function(i){
-						return i.get('trackingStatus') && i.get('ancestry') && i.get('ancestry').indexOf(result.item.get('path')) == 0;
+						return i.get('trackingStatus') && i.get('ancestry') && i.get('ancestry').indexOf(itemRecord.get('path')) == 0;
 					});
-					return Ember.Object.create($.extend(result, {descendants: descendants}))
-				})
-			})
+					return Ember.Object.create({item: itemRecord, descendants: descendants})
+				});
+			});
 	},
 	serialize: function(model) {
 		return { item_path: model.get('item.path').replace(/[\/\s]/g,"_") };
