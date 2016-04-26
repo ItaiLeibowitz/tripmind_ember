58a4e641ed937868cfa5860e54d301d7efc21ec3
itai leibowitz <itai.leibowitz@gmail.com>
2016-04-26 12:31:25 -0400

    added links to the collection being saved

--- app/models/collection.js
+++ app/models/collection.js
@@ -62,56 +62,89 @@ DS.Model.extend({
 	},
 
 	postToServer: function () {
-		var self = this;
-		var serializedRecords = [
-				{
-					attributes: this.toJSON(),
-					type: 'collection',
-					id: this.get('tmToken'),
-					relationships: {
-						items: {
-							data: this.get('items').map(function(item){return {type: 'item', id: item.get('id')}})
-						}
+		var self = this,
+			linksToSend = Ember.ArrayProxy.create({content: []});
+		return this.get('items')
+			.then(function (items) {
+				var potentialLinkPromises = items.map(function (item) {
+					return item.get('potentialLinks');
+				});
+				return Ember.RSVP.all(potentialLinkPromises)
+					.then(function (linkLists) {
+						linkLists.forEach(function(list){
+							linksToSend.addObjects(list);
+						});
+						var serializedRecords = [
+								{
+									attributes: self.toJSON(),
+									type: 'collection',
+									id: self.get('tmToken'),
+									relationships: {
+										items: {
+											data: items.map(function (item) {
+												return {type: 'item', id: item.get('id')}
+											})
+										}
 
-					}
-				}
-			],
-			serializedItems = this.get('items').map(function (item) {
-				return {
-					id: item.get('id'),
-					type: 'item',
-					attributes: item.toJSON(),
-					relationships: {
-						collections: {
-							data: [
-								{type: 'collection', id: self.get('tmToken')}
-							]
-						}
-					}
-				}
+									}
+								}
+							],
+							serializedItems = items.map(function (item) {
+								var potentialLinks = item.get('potentialLinks');
+								return {
+									id: item.get('id'),
+									type: 'item',
+									attributes: item.toJSON(),
+									relationships: {
+										collections: {
+											data: [
+												{type: 'collection', id: self.get('tmToken')}
+											]
+										},
+										potentialLinks: {
+											data: potentialLinks.map(function (link) {
+												return {type: 'potentialLink', id: link.get('id')}
+											})
+										}
+									}
+								}
+							}),
+							serializedLinks = linksToSend.map(function (link) {
+								return {
+									id: link.get('id'),
+									type: 'potentialLink',
+									attributes: link.toJSON(),
+									relationships: {
+										item: {
+											data: {type: 'item', id: link.get('item.id')}
+										}
+									}
+								}
+							});
+						serializedRecords = serializedRecords.concat(serializedItems).concat(serializedLinks);
+						var stringifiedRecords = JSON.stringify(serializedRecords);
+						var compressedJSON = lzwCompress.pack(stringifiedRecords);
+						var compressed = JSON.stringify(compressedJSON).length < stringifiedRecords.length;
+						var compressedData = compressed ? compressedJSON : stringifiedRecords;
+						return headOnlyPromiseFromAjax({
+							url: Constants.BASE_SERVER_URL + '/api/tm/tm_collections/' + self.get('tmToken'),
+							type: 'PATCH',
+							data: {
+								tm_collection: {
+									is_compressed: compressed,
+									last_updated: self.get('updatedAt'),
+									data: compressedData
+								}
+							}
+						}).then(function () {
+							console.log("resolve: ", compressedData)
+							return compressedData
+						}, function (status) {
+							console.log('reject:', status)
+							return status
+						});
+					})
 			});
-		serializedRecords = serializedRecords.concat(serializedItems);
-		var stringifiedRecords = JSON.stringify(serializedRecords);
-		var compressedJSON = lzwCompress.pack(stringifiedRecords);
-		var compressed = JSON.stringify(compressedJSON).length < stringifiedRecords.length;
-		var compressedData = compressed ? compressedJSON : stringifiedRecords;
-		return headOnlyPromiseFromAjax({
-			url: Constants.BASE_SERVER_URL + '/api/tm/tm_collections/' + this.get('tmToken'),
-			type: 'PATCH',
-			data: {
-				tm_collection: {
-					is_compressed: compressed,
-					last_updated: this.get('updatedAt'),
-					data: compressedData
-				}
-			}
-		}).then(function () {
-			console.log("resolve: ", compressedData)
-			return compressedData
-		}, function (status) {
-			console.log('reject:', status)
-			return status
-		});
 	},
 
 	itemsChange:function(){
