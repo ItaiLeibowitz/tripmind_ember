d1cd3ea305ad8d90789364e7638fa1cbb61ba0ed
itai leibowitz <itai.leibowitz@gmail.com>
2016-05-11 16:02:15 -0700

    fix a small bug with detecting Env in style guide

--- app/components/style-guide.js
+++ app/components/style-guide.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
 	// Hide all editing elements on the online version
 	hideEditing: function(){
 		return (ENV.environment === 'production');
-	},
+	}.property(),
 	hasExtension: false,
 	requiredVersion: 1.0,
 
